91b3b43f61e718df10407d974f1f6b07f85706f1
bahazghayar <bahazghayar@hotmail.com>
2021-05-18 00:17:04 +0300

    edit

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -15,11 +15,11 @@ import './todo.scss';
 
 const ToDo = () => {
   const [list, _addItem, _toggleComplete, _getTodoItems, _deleteTask] = useAjax();
-  
+
   useEffect(_getTodoItems, [_getTodoItems]);
 
   useEffect(() => {
-    document.title = "To DO- complete: " +  list.filter((item) => !item.complete).length + "/" + "Incomplete: " + list.filter((item) => item.complete).length; 
+    document.title = "To DO- complete: " + list.filter((item) => !item.complete).length + "/" + "Incomplete: " + list.filter((item) => item.complete).length;
   });
 
   return (
@@ -27,26 +27,26 @@ const ToDo = () => {
       <Navbar class="navBar" expand="lg" variant="dark" bg="dark" style={{ width: '80%', margin: '30px auto 0' }}>
         <Navbar.Brand>There are {list.filter((item) => !item.complete).length} Items To Complete.</Navbar.Brand>
       </Navbar>
-      
+
       <Container fluid="md" style={{ marginTop: '50px' }}>
         <Row className="justify-content-md-center">
-        <Col sm={4}>
-          <Card style={{ width: "18rem" }}>
-            <Card.Body>
-            <Card.Text>
-              <TodoForm handleSubmit={_addItem} />
-              </Card.Text>
-            </Card.Body>
-          </Card>
+          <Col sm={4}>
+            <Card style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Text>
+                  <TodoForm handleSubmit={_addItem} />
+                </Card.Text>
+              </Card.Body>
+            </Card>
           </Col>
-          <Col  md={{ span: 5, offset: 2 }} className="list-item">
-            <TodoList list={list} handleComplete={_toggleComplete}  handleDelete ={_deleteTask}/>
+          <Col md={{ span: 5, offset: 2 }} className="list-item">
+            <TodoList list={list} handleComplete={_toggleComplete} handleDelete={_deleteTask} />
           </Col>
         </Row>
       </Container>
-    </> 
+    </>
   );
-  
+
 }
 
 
